600a48b83fd12acc3f9b3d5d013f57e712bd597d
iggyscode <iggydeveloper@protonmail.com>
2023-07-12 17:39:36 -0500

    scaffolding finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 const App = () => {
+  const directory = [
+    {
+      id: 1,
+      title:'Hat'
+    }
+  ]
   return (
     <div className='categories-container'>
+      {directory.map(({title}) => {
+        return(
+          <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+        )
+      })}
       <div className='category-container'>
         {/* <img /> */}
         <div className='category-body-container'>
