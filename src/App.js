e3a0a6d3850f7a6a54d582366f1bee6dc35f1dcb
iggyscode <iggydeveloper@protonmail.com>
2023-07-23 17:23:57 -0500

    added functional categories component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
+import './components/category-item/category-item.component.jsx'
+import Categories from './components/categories/categories.component.jsx';
+
 const App = () => {
-  const directory = [
-    {
-      id: 1,
-      title:'Hat'
-    }
-  ]
+
+    
   return (
-    <div className='categories-container'>
-      {directory.map(({title}) => {
-        return(
-          <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-        )
-      })}
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+   <Categories />
   );
 };
 
 export default App;
+ 
